11caaa1427ed15a1699115a009c4bda258914877
Caleb Oduro <cpo34@drexel.edu>
2026-04-19 09:58:48 -0400

    updated server permissions

--- backend/server.js
+++ backend/server.js
@@ -449,6 +449,6 @@ app.get("/api/internships", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
